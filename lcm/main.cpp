6a760e2cbf777816478317a3710d6f645a62d5b2
kamol <ckamol7@gmail.com>
2019-06-18 13:04:48 +0600

    Initial Commit

---
+++ lcm/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lcm
+//
+//  Created by Kamol Kumar paul on 18/6/19.
+//  Copyright © 2019 Kamol Kumar paul. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
